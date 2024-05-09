c8a4fa1fe4fccd1f475a902ab8dd041c88e4e516
giovanalm <giovananlm@gmail.com>
2024-05-09 17:14:17 -0300

    Controle de todos os volume ta on fire meu fiiii

--- 2024-303-ANTENA2/Assets/Scripts/ControleVolume.cs
+++ 2024-303-ANTENA2/Assets/Scripts/ControleVolume.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class ControleVolume : MonoBehaviour
 {
-    [SerializeField] AudioSource musicaFundo;
-    public void VolumeMusica(float valor){
-        musicaFundo.volume = valor;
+    public float volumeGeral, volumeMusica, volumeEfeito;
+    public void VolumeGeral(float volume){
+        volumeGeral = volume;
+        AudioListener.volume = volumeGeral;
+    }
+
+    public void VolumeMusica(float volume){
+        volumeMusica = volume;
+        GameObject[] Musicas = GameObject.FindGameObjectsWithTag("Musica");
+        for(int i = 0; i < Musicas.Length; i++){
+            Musicas[i].GetComponent<AudioSource>().volume = volumeMusica;
+        }
+    }
+
+    public void VolumeEfeitos(float volume){
+        volumeEfeito = volume;
+        GameObject[] Efeitos = GameObject.FindGameObjectsWithTag("Efeitos");
+        for(int i = 0; i < Efeitos.Length; i++){
+            Efeitos[i].GetComponent<AudioSource>().volume = volumeEfeito;
+        }
     }
 }
