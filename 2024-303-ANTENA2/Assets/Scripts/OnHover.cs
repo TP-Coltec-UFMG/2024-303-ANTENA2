5eaa03c1ae8a1e8b4d993a47c3061ea8d9cbdb5d
giovanalm <giovananlm@gmail.com>
2024-05-17 22:52:51 -0300

    mais botoes daltonicos

--- 2024-303-ANTENA2/Assets/Scripts/OnHover.cs
+++ 2024-303-ANTENA2/Assets/Scripts/OnHover.cs
@@ -2,55 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Timeline;
 using UnityEngine.UI;
 
 public class OnHover : MonoBehaviour
 {
+    private SpriteRenderer rend;
+    private Sprite protSprite, tritSprite, deutSprite, nadaSprite;
     public Botao botoes;
     public int filtroAtual;
 
-    public void OnMouseEnter() {
-        filtroAtual = Colorblind.Type;
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        if (rend == null)
+        {
+            Debug.LogError("SpriteRenderer component not found on " + gameObject.name);
+        }
+        
+        /*protSprite = Resources.Load<Sprite>("Images/Configuracoes/hover0");
+        tritSprite = Resources.Load<Sprite>("Images/Configuracoes/hover0"); // Provide correct path
+        deutSprite = Resources.Load<Sprite>("Images/Configuracoes/hover0"); // Provide correct path
+        nadaSprite = Resources.Load<Sprite>("Images/Configuracoes/hover0"); // Provide correct path
 
-        switch(botoes) {
-            case Botao.Deuteranopia:
-                Colorblind.Type = 2;
-                break;
-            case Botao.Protanopia:
-                Colorblind.Type = 1;
-                break;
-            case Botao.Tritanopia:
-                Colorblind.Type = 3;
-                break;
-            case Botao.SemFiltro:
-                Colorblind.Type = 0;
-                break;
+        if (protSprite == null || tritSprite == null || deutSprite == null || nadaSprite == null)
+        {
+            Debug.LogError("One or more sprites could not be loaded. Check the paths.");
+        }*/
+    }
 
-            default:
-                break;
+    void OnMouseEnter() 
+    {
+        if (rend != null)
+        {
+            rend.enabled = true;
+            
+            /*switch (botoes) 
+            {
+                case Botao.Deuteranopia:
+                    rend.sprite = deutSprite;
+                    Debug.Log("Deuteranopia sprite set.");
+                    break;
+                case Botao.Protanopia:
+                    rend.sprite = protSprite;
+                    Debug.Log("Protanopia sprite set.");
+                    break;
+                case Botao.Tritanopia:
+                    rend.sprite = tritSprite;
+                    Debug.Log("Tritanopia sprite set.");
+                    break;
+                case Botao.SemFiltro:
+                    rend.sprite = nadaSprite;
+                    Debug.Log("SemFiltro sprite set.");
+                    break;
+                default:
+                    rend.sprite = nadaSprite;
+                    Debug.Log("Default sprite set.");
+                    break;
+            }*/
         }
     }
 
-    public void OnMouseExit() {
-        Colorblind.Type = filtroAtual;
+    void OnMouseExit() 
+    {
+        if (rend != null)
+        {
+            rend.enabled = false;
+            Debug.Log("SpriteRenderer disabled.");
+        }
     }
 
-    public enum Botao{
+    public enum Botao
+    {
         Deuteranopia,
         Protanopia,
         Tritanopia,
         SemFiltro
     }
-    /*
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }*/
 }
