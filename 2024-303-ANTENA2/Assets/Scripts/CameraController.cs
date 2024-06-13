9e4aaf9cdd4b19c9f3d330879fa78bea97598ffb
giovanalm <giovananlm@gmail.com>
2024-06-13 19:29:42 -0300

    camera limitada para o espaço da casa

--- 2024-303-ANTENA2/Assets/Scripts/CameraController.cs
+++ 2024-303-ANTENA2/Assets/Scripts/CameraController.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target; //para colocarmos nosso personagem como alvo
-    void Update(){
+    private float smoothing = 0.01f;
+    /*void Update(){
     //vamos fazer a câmera seguir o personagem em x e y, mas manter o seu z.
         transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+    }*/
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
+    void LateUpdate(){
+        if (transform.position != target.position){
+            Vector3 targetPosition = new Vector3(target.transform.position.x,target.transform.position.y, transform.position.z);
+
+    //clamp in between
+        targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+
+    //gradualmente move o objeto: posição original, posição alvo, velocidade
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+    }
+
+
     }
 
 }
