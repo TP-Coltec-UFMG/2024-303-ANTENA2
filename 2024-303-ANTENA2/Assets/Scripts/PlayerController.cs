61e8e1834c9d0f0ec363d70c2a00a72030369296
giovanalm <giovananlm@gmail.com>
2024-06-13 21:01:12 -0300

    animação do personagem que não funciona

--- 2024-303-ANTENA2/Assets/Scripts/PlayerController.cs
+++ 2024-303-ANTENA2/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,23 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D player;
     [SerializeField] private float speed;
-    // Start is called before the first frame update
+    private Animator myAnimation;
+
     void Start(){
         player = GetComponent<Rigidbody2D>();
+        myAnimation = GetComponent<Animator>();
     }
     void Update(){
-        player.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
+        player.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
+        myAnimation.SetFloat("moveX", player.velocity.x);
+        myAnimation.SetFloat("moveY", player.velocity.y);
+
+
+//Se estivermos para a direção X, nosso último movimento será para X
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 ||Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1){
+            myAnimation.SetFloat("lastmoveX", Input.GetAxisRaw("Horizontal"));
+            myAnimation.SetFloat("lastmoveY", Input.GetAxisRaw("Vertical"));
+
+        }
     }
 }
