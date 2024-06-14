ab0976eb2d94bef855c67dfeca84ae122fd735a6
Hiaaf <83770823+Hiaaf@users.noreply.github.com>
2024-06-13 22:17:30 -0300

    Aparece previa da mensagem

--- 2024-303-ANTENA2/Assets/Scripts/Cifras/Mensagens Chegando/MensagemNova.cs
+++ 2024-303-ANTENA2/Assets/Scripts/Cifras/Mensagens Chegando/MensagemNova.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class MensagemNova : MonoBehaviour
 {
     private MensagemSO _mensagem;
+    [SerializeField] private TextMeshPro text;
 
+    
     public void SetMensagem(MensagemSO mensagem)
     {
         _mensagem = mensagem;
+
+        const int numChars = 15;
+        text.text = mensagem.mensagem[..numChars] + "...";
     }
 }
