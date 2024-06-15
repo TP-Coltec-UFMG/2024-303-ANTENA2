3a88f62240a757d8aaababa0a88849fec18d6b6d
R3nn4 <joao.rwt@gmail.com>
2024-06-15 14:31:09 -0300

    Mudanças aí nas coisa

--- 2024-303-ANTENA2/Assets/Scripts/Frequencias.cs
+++ 2024-303-ANTENA2/Assets/Scripts/Frequencias.cs
@@ -1,27 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Frequencias : MonoBehaviour
 {
     [SerializeField] public Slider slider;
-    [SerializeField] public TextMeshPro textoFrequencia;
+    [SerializeField] public TextMeshProUGUI textoFrequencia;
     private float frequencia;
 
     // Start is called before the first frame update
     void Start()
     {
-        textoFrequencia = GameObject.FindGameObjectWithTag("TextoFrequencia").GetComponent<TextMeshPro>();
+        Debug.Log(textoFrequencia.gameObject.name);
     }
 
     // Update is called once per frame
     void Update()
     {
-        frequencia = slider.value + 100;
-        textoFrequencia.text = frequencia.ToString();
+        frequencia = slider.value + 100.0f;
+        int aux = (int)(frequencia * 10);
+        string aux2 = aux.ToString();
+        //aux2.Insert(3, "        ");
+
+        Debug.Log(aux);
         Debug.Log(frequencia);
+        textoFrequencia.text = aux2;
     }
 
     public bool checaFrequencia(float frequenciaExterna)
