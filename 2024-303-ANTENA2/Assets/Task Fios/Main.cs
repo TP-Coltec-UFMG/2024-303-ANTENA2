0cae9fbda62ead04ce9ca0c7779fa5ec0407b1ee
Giovana Nassif Leonel Membrive <giovananlm@gmail.com>
2024-06-17 09:44:50 -0300

    Fios

---
+++ 2024-303-ANTENA2/Assets/Task Fios/Main.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    static public Main Instance;
+    public int objectsCount;
+    public GameObject winText;
+    private int onCount = 0;
+    private void Awake(){
+        Instance = this;
+    }
+    public void SwitchChange(int points){
+        onCount = onCount + points;
+        if(onCount == objectsCount){
+            winText.SetActive(true);
+        }
+    }
+}
---
+++ 2024-303-ANTENA2/Assets/Task Fios/Wire.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Wire : MonoBehaviour
+{
+    public SpriteRenderer wireEnd;
+    public GameObject lightOn;
+    Vector3 startPoint;
+    Vector3 startPosition;
+    void Start()
+    {
+        startPoint = transform.parent.position;
+        startPosition = transform.position;
+    }
+
+    private void OnMouseDrag(){
+        //posição do mouse
+        Vector3 newPosition = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0;
+
+        //checar se existe conexão por perto
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(newPosition, .2f);
+
+        foreach(Collider2D collider in colliders){
+            //olhar se não é seu prórpio collider
+            if(collider.gameObject != gameObject){
+                //atualiza o fio para ficar no ponto de conexão
+                UpdateWire(collider.transform.position);
+                //checar se os fios tem a mesma cor
+                if(transform.parent.name.Equals(collider.transform.parent.name)){
+                    //contar fio como feito
+                    Main.Instance.SwitchChange(1);
+                    //terminado
+                    collider.GetComponent<Wire>()?.Done();
+                    Done();
+                }
+                return;
+            }
+        }
+
+        //atualizar o fio
+        UpdateWire(newPosition);
+        
+    }
+    private void Done(){
+        //ligar as luzinhas
+        lightOn.SetActive(true);
+
+        //destruir o script
+        Destroy(this);
+    }
+    private void OnMouseUp(){
+        //reset wire position
+        UpdateWire(startPosition);
+    }
+
+    private void UpdateWire(Vector3 newPosition){
+        //update wire and position
+        transform.position = newPosition;
+
+        //update direction
+        Vector3 direction = newPosition - startPoint;
+        transform.right = direction * transform.lossyScale.x;
+
+        //update scale
+        float distance = Vector2.Distance(startPoint, newPosition);
+        wireEnd.size = new Vector2(distance, wireEnd.size.y);
+    }
+}
