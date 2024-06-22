872b45bb79ef4b2bc6dbbafb3739e9c45417fdc7
Hiaaf <83770823+Hiaaf@users.noreply.github.com>
2024-06-22 17:52:26 -0300

    Fiz coiso de daz zoom na telinha

--- 2024-303-ANTENA2/Assets/Scripts/Mesa/CameraLookAt.cs
+++ 2024-303-ANTENA2/Assets/Scripts/Mesa/CameraLookAt.cs
@@ -8,14 +8,20 @@ using UnityEngine.Serialization;
 public class CameraLookAt : MonoBehaviour
 {
     private CinemachineVirtualCamera _cvc;
+    private float _cvcOrthoSizeDefault;
     [SerializeField] private Locais currentFollow;
+    private Transform _currentFollowTransform;
     [SerializeField] private Transform transmissaoTransform;
     [SerializeField] private Transform maletaTransform;
+    private const float TvMinigamesOrthoSize = .5f;
+    [SerializeField] private Transform tvMinigamesTransform;
 
 
     private void Start()
     {
         _cvc = GetComponent<CinemachineVirtualCamera>();
+        _cvcOrthoSizeDefault = _cvc.m_Lens.OrthographicSize;
+        _currentFollowTransform = _cvc.Follow;
     }
 
     private void Update()
@@ -23,30 +29,59 @@ public class CameraLookAt : MonoBehaviour
         HandleInput();
     }
 
-    public void LookAt(Locais local)
+    private IEnumerator ChangeOrthoSize(float orthoSize)
     {
+        float curOrthoSize = _cvc.m_Lens.OrthographicSize;
+        const float duration = .5f;
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            _cvc.m_Lens.OrthographicSize = Mathf.Lerp(curOrthoSize, orthoSize, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        _cvc.m_Lens.OrthographicSize = orthoSize;
+    }
+    
+    public void Follow(Locais local, float? orthoSize)
+    {
+        StartCoroutine(ChangeOrthoSize(orthoSize ?? _cvcOrthoSizeDefault));
         currentFollow = local;
 
-        _cvc.Follow = currentFollow switch
+        _cvc.Follow = _currentFollowTransform = currentFollow switch
         {
             Locais.Transmissao => transmissaoTransform,
             Locais.Maleta => maletaTransform,
+            Locais.TVminigames => tvMinigamesTransform,
             _ => _cvc.Follow
         };
     }
 
     private void HandleInput()
     {
+        #region Teste TVminigame
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (currentFollow == Locais.TVminigames)
+                Follow(Locais.Transmissao, null);
+            else
+                Follow(Locais.TVminigames, TvMinigamesOrthoSize);
+        }
+        
+        #endregion
+        
         switch (currentFollow)
         {
             case Locais.Transmissao:
                 if (Input.GetAxisRaw("Horizontal") > 0) // Pediu pra olhar pra direita
-                    LookAt(Locais.Maleta);
+                    Follow(Locais.Maleta, null);
                 break;
             
             case Locais.Maleta:
                 if (Input.GetAxisRaw("Horizontal") < 0) // Pediu pra olhar pra esquerda
-                    LookAt(Locais.Transmissao);
+                    Follow(Locais.Transmissao, null);
                 break;
         }
     }
@@ -54,6 +89,7 @@ public class CameraLookAt : MonoBehaviour
     public enum Locais
     {
         Transmissao,
-        Maleta
+        Maleta,
+        TVminigames
     }
 }
