526cd1b6350231c51d7db49ccb2f11f456c6a74d
Hiaaf <83770823+Hiaaf@users.noreply.github.com>
2024-06-28 15:36:04 -0300

    As cifra sobe suavemente agora ao inves d teleportar

--- 2024-303-ANTENA2/Assets/Scripts/Mesa/CifrasMesa.cs
+++ 2024-303-ANTENA2/Assets/Scripts/Mesa/CifrasMesa.cs
@@ -7,6 +7,7 @@ public class CifrasMesa : MouseInteractive
 {
     private Vector3 _initialPos;
     private Vector3 _initialITHpos;
+    private bool _isCoroutinePlaying;
 
 
     private new void Start()
@@ -19,19 +20,41 @@ public class CifrasMesa : MouseInteractive
     
     private void Update()
     {
-        const float upAmount = .03f;
+        const float upAmount = .035f;
 
-        Vector3 pos = transform.position;
-        if (MouseOver())
+        if (MouseOver() && !_isCoroutinePlaying)
         {
-            pos.y = _initialPos.y + upAmount;
+            StartCoroutine(MoveVerticalSmooth(_initialPos.y + upAmount));
         }
-        else
+        else if (!MouseOver() && !_isCoroutinePlaying)
         {
-            pos = _initialPos;
+            StartCoroutine(MoveVerticalSmooth(_initialPos.y));
         }
 
-        transform.position = pos;
         imgToHighlight.transform.position = _initialITHpos;
     }
+    
+    private IEnumerator MoveVerticalSmooth(float finalYPos)
+    {
+        _isCoroutinePlaying = true;
+        
+        float curYPos = transform.position.y;
+        const float duration = .1f;
+        float elapsed = 0f;
+        Vector3 pos = transform.position;
+        while (elapsed < duration)
+        {
+            pos = transform.position;
+            float newY = Mathf.Lerp(curYPos, finalYPos, elapsed / duration);
+            pos.y = newY;
+            transform.position = pos;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        _isCoroutinePlaying = false;
+        pos.y = finalYPos;
+        transform.position = pos;
+    }
+
 }
