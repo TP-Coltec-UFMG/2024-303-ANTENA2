5cb5d2aba34145745ef551def96cff42928910a1
giovananlm <giovananlm@gmail.com>
2024-08-07 10:43:09 -0300

    Troca de cenas: da mesa para a casa e da casa para a mesa. Seria interessante acrescentar transição entre as cenas, como "Fim do turno de trabalho" e "Dia 2"

---
+++ 2024-303-ANTENA2/Assets/Scripts/Casa/PassaDia.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PassaDia : MonoBehaviour {
+    public int idCena = 2;
+    public string cena = "Mesa";
+
+    void Update(){}
+    void OnTriggerEnter2D (Collider2D collider) {
+        SceneManager.LoadScene(cena);
+    }
+}
--- 2024-303-ANTENA2/Assets/Scripts/Mesa/TimeManager.cs
+++ 2024-303-ANTENA2/Assets/Scripts/Mesa/TimeManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using TMPro;
 
@@ -11,15 +12,21 @@ public class TimeManager : MonoBehaviour {
     [Header("How fast Time Should Pass")]
     [SerializeField] private float timeScale = 2.0f;
     private float elapseTime;
+    private string nomeCena = "Casa";
 
     private void Start() {
-        elapseTime = 9 * 3600f; //horário que começa o turno
+        elapseTime = 9 * 3600f; //9h horário que começa o turno
     }
 
     private void Update() {
         elapseTime += Time.deltaTime * timeScale;
         elapseTime %= timeInADay;
         UpdateClockUI();
+
+        // Às 18h acaba o turno de trabalho e troca para a cena da casa
+        if (elapseTime >= (18 * 3600)){
+            ChangeScene(1);
+        }
     }
 
     void UpdateClockUI(){
@@ -27,7 +34,11 @@ public class TimeManager : MonoBehaviour {
         int minutes = Mathf.FloorToInt((elapseTime - hours * 3600f) / 60f);
         int seconds = Mathf.FloorToInt((elapseTime - hours * 3600f) - ( minutes * 60f));
 
-        string clockString = string.Format("{00:00}:{01:00}", hours, minutes);
+        string clockString = string.Format("{00:00}:00", hours);
         clockText.text = clockString;
+    }
+    public void ChangeScene(int num)
+    {
+        SceneManager.LoadScene(num);
     }
 }
