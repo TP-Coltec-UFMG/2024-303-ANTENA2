255c9803148dcbc4b110703063df49fed1f675bf
giovanalm <giovananlm@gmail.com>
2024-08-10 21:41:38 -0300

    Animações fade in e fade out do dia, não funcionantes ainda

--- 2024-303-ANTENA2/Assets/Scripts/Casa/PassaDia.cs
+++ 2024-303-ANTENA2/Assets/Scripts/Casa/PassaDia.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PassaDia : MonoBehaviour {
     public int idCena = 2;
     public string cena = "Mesa";
+    [SerializeField] private TMP_Text textoDiaTrans;
+    //textoDiaTrans.text = dia.ToString();
+
+    private void Start(){
+        //fade out do fim trabaio?
+    }
 
     void Update(){}
     void OnTriggerEnter2D (Collider2D collider) {
         //meter fadeIn do dia
         SceneManager.LoadScene(cena);
-        //meter fadeOut do dia
         GameHandler.Dia ++;
     }
 }
