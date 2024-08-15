35e8c5c6a23d2a6e8fba78c96e5cd8e715e731ce
Hiaaf <83770823+Hiaaf@users.noreply.github.com>
2024-08-15 10:38:18 -0300

    Coisinhos FadeinOut nana <3

--- 2024-303-ANTENA2/Assets/Scripts/Casa/PassaDia.cs
+++ 2024-303-ANTENA2/Assets/Scripts/Casa/PassaDia.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
 public class PassaDia : MonoBehaviour {
     public int idCena = 2;
     public string cena = "Mesa";
     [SerializeField] private TMP_Text textoDiaTrans;
+
+    [SerializeField] private GameObject fadeInDia;
+    [SerializeField] private Animator fadeIntrabaioa;
     //textoDiaTrans.text = dia.ToString();
 
-    private void Start(){
+    private bool _isAnimationPlaying = false;
+    
+    private void Start() {
         //fade out do fim trabaio?
+        fadeIntrabaioa.Play("wdiopawjdoian");
     }
 
-    void Update(){}
+    private void Update()
+    {
+        if (_isAnimationPlaying)
+        {
+            fadeInDia.GetComponent<Animator>();//.IsEnded();
+            SceneManager.LoadScene(cena);
+            GameHandler.Dia ++;
+        }
+    }
+    
     void OnTriggerEnter2D (Collider2D collider) {
         //meter fadeIn do dia
-        SceneManager.LoadScene(cena);
-        GameHandler.Dia ++;
+        fadeInDia.GetComponent<Animator>().Play("WUDHBAWUIODB");
+        _isAnimationPlaying = true;
+        // SceneManager.LoadScene(cena);
+        // GameHandler.Dia ++;
     }
 }
