dd2c73effbf36d951e5c6e12bb0bec7e645ad475
gabi <damasogabriela79@gmail.com>
2024-08-28 17:15:03 -0300

    n ta funfando

--- 2024-303-ANTENA2/Assets/Scripts/Casa/TextosAntigoOperador/DocumentosEncontrados.cs
+++ 2024-303-ANTENA2/Assets/Scripts/Casa/TextosAntigoOperador/DocumentosEncontrados.cs
@@ -7,10 +7,25 @@ public class DocumentosEncontrados : MonoBehaviour {
     [SerializeField] EscritosSO documentos;
     [SerializeField] TextMeshProUGUI textObject;
     [SerializeField] GameObject fundoDoc;
+    [SerializeField] GameObject fundoPreto;
 
-    void AbreDoc() {
+    private int i;
+
+    public void AbreDoc() { 
+        i = 0;
+        fundoPreto.SetActive(false);
         fundoDoc.SetActive(true);
-        textObject.text = documentos.escritosDocs[1].escrito;
+        Debug.Log(i);
+
+        while(documentos.escritosDocs[i].dia != 1 && i < documentos.escritosDocs.Count) {
+            Debug.Log(i);
+            i++;
+        }
+        if(documentos.escritosDocs[i].dia == 1) {
+            textObject.text = documentos.escritosDocs[i].escrito;
+            Debug.Log(i);
+            Debug.Log(documentos.escritosDocs[i].escrito);
+        }
     }
 
     void Start()
