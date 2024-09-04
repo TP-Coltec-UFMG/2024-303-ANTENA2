fabfe936d40adbebe5d46b3e2046977ba826a939
Gabriela Dâmaso <damasogabriela79@gmail.com>
2024-09-03 21:20:16 -0300

    agr a mesa para quando os jogos começam

--- 2024-303-ANTENA2/Assets/Scripts/Mesa/MiniGameMemoria/PaneNaMesa.cs
+++ 2024-303-ANTENA2/Assets/Scripts/Mesa/MiniGameMemoria/PaneNaMesa.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PaneNaMesa : MonoBehaviour {
     [SerializeField] GameObject Relogio;
     private PausaTempo pausaTempo;
+    
+    [SerializeField] GameObject Slots;
+    [SerializeField] GameObject editorCentral;
 
     void Awake() {
         pausaTempo = Relogio.GetComponent<PausaTempo>();
@@ -12,12 +15,26 @@ public class PaneNaMesa : MonoBehaviour {
     public void desligaMesa() {
         pausaTempo.PausarJogo();
         GameHandler.Instance.PausarMensagens(true);
-        Debug.Log("pausou");
+        Slots.SetActive(false);
+        DeactivateImmediateChildrenOnly(editorCentral);
     }
     public void ligaMesa() {
         pausaTempo.PausarJogo();
         GameHandler.Instance.PausarMensagens(false);
-        Debug.Log("Despausou");
+        Slots.SetActive(true);
+        ActivateImmediateChildrenOnly(editorCentral);
     }
 
+    public void DeactivateImmediateChildrenOnly(GameObject parentObject) {
+        foreach (Transform child in parentObject.transform) {
+            child.gameObject.SetActive(false);
+        }
+    }
+    public void ActivateImmediateChildrenOnly(GameObject parentObject) {
+        foreach (Transform child in parentObject.transform) {
+            child.gameObject.SetActive(true);
+        }
+    }
+    
 }
+
