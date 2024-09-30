711da3a24c672b581ab7606ba654d57879bc04ff
Gabriela Dâmaso <damasogabriela79@gmail.com>
2024-09-30 02:30:46 -0300

    Correcoes finais aí

--- 2024-303-ANTENA2/Assets/Scripts/Dia4/AtivadorDia4.cs
+++ 2024-303-ANTENA2/Assets/Scripts/Dia4/AtivadorDia4.cs
@@ -10,7 +10,9 @@ public class AtivadorDia4 : MonoBehaviour {
     [SerializeField] private float verticalRange; // adjust this value to change the vertical detection range
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject textBox;
+    [SerializeField] private GameObject textBox2;
     private bool eventoTerminado;
+    private float tempo2 = 5f;
     void Start() {
         if(GameHandler.Dia == 4) {
             Debug.Log("Dia 4");
@@ -23,6 +25,15 @@ public class AtivadorDia4 : MonoBehaviour {
                 tempo -= Time.deltaTime;
             }
             else if (tempo <= 0.0f) {
+                Debug.Log("[É pra tá batendo na porta]");
+                textBox2.SetActive(true);
+                if (tempo2 >= 0.0f) {
+                    tempo2 -= Time.deltaTime;
+                }
+                else if (tempo2 <= 0.0f) {
+                    textBox2.SetActive(false);
+                }
+
                 float horizontalDistance = Mathf.Abs(transform.position.x - player.transform.position.x);
                 float verticalDistance = Mathf.Abs(transform.position.y - player.transform.position.y);
                 
@@ -31,6 +42,9 @@ public class AtivadorDia4 : MonoBehaviour {
                         eventoTerminado = true;
                         textBox.SetActive(true);
                     }
+                }
+                else {
+                    textBox.SetActive(false);
                 }
             } 
         }
